eb96ffbbe7773333c6206d238660651e704b0aab
Chea Ilong <155554832+Chea-Ilong@users.noreply.github.com>
2025-03-16 19:06:34 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-red text-2xl">
+      Hello, Tailwind!
+    </div>
+  );
+}
