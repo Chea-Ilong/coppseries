fdf37881ff3c231d36aad17f481b6b105021db16
nithkidd <123559135+nithkidd@users.noreply.github.com>
2025-03-17 21:50:05 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css' 
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
