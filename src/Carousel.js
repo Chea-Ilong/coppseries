af3dc9e7578c4cd261db5d2507243ade13fdfec1
nithkidd <123559135+nithkidd@users.noreply.github.com>
2025-03-19 23:05:19 +0700

    carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,20 +4,20 @@ export const slides = [
     alt: "Geometric Art",
     title: "Modern Geometric Art",
     description: "Explore the fascinating world of geometric patterns and neon aesthetics in this stunning visual experience.",
-    href: "/geometric-art"
+    href: "#"
   },
   {
     src: "https://img.freepik.com/free-photo/nighttime-nature-landscape-galaxy-mountain-water-star-beauty-generative-ai_188544-9736.jpg?t=st=1742398421~exp=1742402021~hmac=fbbc76fbadb440b6996d654dfa179d7d1e1f448705806b9802a29aa192468a27&w=1060",
     alt: "Night Sky",
     title: "Celestial Wonders",
     description: "Witness the breathtaking beauty of the night sky as it meets the serene mountain landscape.",
-    href: "/celestial-wonders"
+    href: "#"
   },
   {
     src: "https://img.freepik.com/premium-photo/purple-landscape-with-circle-middle-forest-background_896008-7.jpg?w=1060",
     alt: "Purple Forest",
     title: "Mystical Forest",
     description: "Journey through an enchanted forest bathed in ethereal purple light, where magic comes alive.",
-    href: "/mystical-forest"
+    href: "#"
   }
 ];
