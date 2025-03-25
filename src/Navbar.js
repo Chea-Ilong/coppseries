15e3e0040b9f4f8e4de612f83375e81e5e463d7b
nithkidd <123559135+nithkidd@users.noreply.github.com>
2025-03-25 15:28:21 +0700

    fix(navbar): update href paths for Shop and About Us

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 export const navItems = [
     { name: "Home", href: "/" },
-    { name: "Shop", href: "/shop" },
-    { name: "About Us", href: "/about" },
+    { name: "Shop", href: "shop" },
+    { name: "About Us", href: "about" },
     { name: "Contact Us", href: "contact" },
   ];
